e7d958a4e699d769d776d1a63fcabbd5dd6d5641
manish221298 <iammnsh01@gmail.com>
2020-09-10 23:00:38 +0530

    commit

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:3055/'
+    //baseURL: 'http://localhost:3055/'
+    baseURL: '/'
 })
 
 export default axios;
